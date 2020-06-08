a101faf1c994dc9638d456bfd72b0f1bd453dc93
David <dbutler4@hawk.iit.edu>
2020-06-08 14:46:54 -0500

    Renders correctly and reacts to touch

---
+++ app/sections/Header.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image} from 'react-native';
+
+export class Header extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {isLoggedIn: false};
+    }
+
+    toggleUser = ()=>{
+        this.setState(previousState => {
+            return {isLoggedIn: !previousState.isLoggedIn };
+        });
+    }
+
+    render(){
+        let display = this.state.isLoggedIn ? 'Sampler User' : this.props.message ;
+        return (
+            <View style = {styles.headStyle}>
+                <Text 
+                    style={styles.headText}
+                    onPress={this.toggleUser}>{display}
+                </Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    headText: {
+        textAlign: 'right',
+        color: '#ffffff',
+        fontSize: 20
+    },
+    headStyle: {
+        paddingTop: 30,
+        paddingRight: 10,
+        backgroundColor: '#35605a',
+        flex: 1
+    }
+})
---
+++ app/views/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View} from 'react-native';
+import {Header} from '../sections/Header.js';
+
+export class Home extends React.Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Header message = 'Press to Login' />
+                <Text style = {{flex:8}}>This will be the Homepage</Text>
+                <Text style={{flex:6}}>These other lines are here</Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    }
+});
